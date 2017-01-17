3479e46259b28e39eaf4c375cabe6e2ec804c794
Drake Bennion <drakebennion@gmail.com>
2017-01-16 20:00:57 -0700

    didn't know i could just say range(len...) also I'm considering switching to golang. shrug

--- Easy295.py
+++ Easy295.py
@@ -4,7 +4,7 @@ def change_letters(from_string, to_string):
 	print from_string
 	letters = list(from_string)
 
-	for i in range(0, len(letters)):
+	for i in range(len(letters)):
 		if (letters[i] != to_string[i]):
 			letters[i] = to_string[i]
 			print "".join(letters)
